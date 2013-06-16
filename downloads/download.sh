f0f95b9a2cf6f4c4f0a7fa26b9b7021b8067baff
Philip Durbin <philipdurbin@gmail.com>
2013-06-16 11:47:17 -0400

    final versions of netbeans 7.3.1 and glassfish 4

--- downloads/download.sh
+++ downloads/download.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-curl -L -O http://download.netbeans.org/netbeans/7.3/final/zip/netbeans-7.3-201302132200.zip
-# https://blogs.oracle.com/arungupta/entry/glassfish_4_latest_builds_zip
-curl -L -O http://download.java.net/glassfish/4.0/promoted/glassfish-4.0-b83.zip
+curl -L -O http://download.netbeans.org/netbeans/7.3.1/final/zip/netbeans-7.3.1-201306052037.zip
+curl -L -O http://download.java.net/glassfish/4.0/release/glassfish-4.0.zip
